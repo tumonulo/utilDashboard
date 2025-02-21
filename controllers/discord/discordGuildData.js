e8ba5d6271ad9818582a9ee0661e883e954331c7
tumonulo <166932691+tumonulo@users.noreply.github.com>
2025-02-21 13:24:17 +0100

    nkj

--- controllers/discord/discordGuildData.js
+++ controllers/discord/discordGuildData.js
@@ -16,16 +16,17 @@ module.exports = async function discordGuilds(req, res) {
             channels.push({ 'id': channel.id, 'name': channel.name, 'type': channel.type })
         })
 
+        const boosts = guild.premiumSubscriptionCount
         const totalMembers = guild.memberCount
-
         const members = await guild.members.fetch({ withPresences: true })
         const activeMembers = members.filter(member =>
           member.presence?.status === 'online' ||
           member.presence?.status === 'dnd' ||
           member.presence?.status === 'idle'
-        );
+        )
     
         res.json({
+            'boosts': boosts,
             'members': { total: totalMembers, active: activeMembers.size }
             'channels': channels
         })
