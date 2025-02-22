417722938c573fde639091e54e36240caff4fda5
tumonulo <juegosadrian777@gmail.com>
2025-02-22 17:38:28 +0100

    aña

--- public/js/loadGuildChannelData.js
+++ public/js/loadGuildChannelData.js
@@ -9,21 +9,21 @@ async function loadChannels() {
     try {
         const pathSplited = window.location.pathname.split('/')
         const guildID = pathSplited[2]
-        const channelID = pathSplited[4]
-console.log(pathSplited,channelID)
+        const channelID = pathSplited[3]
+
         const response = await fetch(`/discord/${guildID}/${channelID}/data`)
         const data = await response.json()
 
 
         messageHeaderChannel.textContent = data.channel.name
 
-        for (const [id, message] of data.messages) {
+        for (const [id, message] of Object.entries(data.messages)) {
             const messageDiv = document.createElement('div')
             messageDiv.classList.add('message')
             messageDiv.textContent = message.content
             messagesDiv.appendChild(messageDiv)
         }
     } catch (error) {
-        console.error('Error al cargar los servidores', error)
+        console.error('Error al cargar los mensajes', error)
     }
 }
